30040d4c550773bf38032d698b754c43500ce64a
VINANK <neupaneayush3@gmail.com>
2023-09-30 17:43:38 +0545

    added profilefeed models

--- profiles_project/profiles_api/models.py
+++ profiles_project/profiles_api/models.py
@@ -66,3 +66,12 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
 		return self.email
 
 
+class ProfileFeedItem(models.Model):
+	""""profile status update"""
+	user_profile=models.ForeignKey('UserProfile',on_delete=models.CASCADE)
+	status_text=models.CharField(max_length=255)
+	created_on=models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		"""returns the models as a string"""
+		return self.status_text
